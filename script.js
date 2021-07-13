f7a9c91ff6e2ebeb5816fd070b2ff94119104448
Steve Corwin <37982371+PrinceCorwin@users.noreply.github.com>
2021-07-12 20:25:02 -0500

    updated navbar and footer

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// navbar script
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
@@ -8,4 +7,22 @@ hamburger.addEventListener("click", () => {
   links.forEach((link) => {
     link.classList.toggle("fade");
   });
+});
+// event listener for help and hide images buttons
+let imgVisibility = "show";
+$("#help-btn").click(function () {
+  $("#help").toggle();
+});
+$(".fa-times-circle").click(function () {
+  $("#help").toggle();
+});
+$("#hide-btn").click(function () {
+  if (imgVisibility === "show") {
+    $("#hide-btn-txt").text("Show Images");
+    imgVisibility = "hide";
+  } else {
+    $("#hide-btn-txt").text("Hide Images");
+    imgVisibility = "show";
+  }
+  $(".pipe-img").toggle();
 });
